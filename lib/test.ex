e9daf7cdb464e6edd35480e3610a2d161a65d184
Blake Kostner <git@btkostner.io>
2021-07-27 13:24:13 -0600

    more failure cases

--- lib/test.ex
+++ lib/test.ex
@@ -1,3 +1,7 @@
+defmodule Testerr do
+  defexception message: "testing balblabla"
+end
+
 defmodule Test do
   @moduledoc """
   Documentation for Test.
@@ -14,7 +18,21 @@ defmodule Test do
   """
   @spec hello(nil) :: Version.t()
   def hello (arg) do
+    IO.inspect("test", label: "credo warning")
+
+    unless true do
+      "test"
+    else
+      "rawr"
+    end
+
     case arg do
+      "world" -> 0
+      "world" -> 0
+      "world" -> 0
+      "world" -> 0
+      "world" -> 0
+      "world" -> 0
       "world" -> 0
       "world" -> 0
       "world" -> 0
@@ -44,5 +62,10 @@ defmodule Test do
       "world" -> 0
       "hello" -> "hello"
     end
+  end
+
+  @spec fnt(String.t()) :: :ok
+  def fnt(arg) when is_binary(arg) do
+    :ok
   end
 end
